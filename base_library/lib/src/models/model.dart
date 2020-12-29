fa09180077b3a0392f70c3f33b4d2b365a68661c
xujiankun <jiankunxu@126.com>
2020-12-29 12:31:36 +0800

    update

--- base_library/lib/src/models/model.dart
+++ base_library/lib/src/models/model.dart
@@ -30,3 +30,68 @@ class VersionModel {
     return sb.toString();
   }
 }
+class LanguageModel {
+  String titleId;
+  String languageCode;
+  String countryCode;
+  bool isSelected;
+
+  LanguageModel(this.titleId, this.languageCode, this.countryCode,
+      {this.isSelected: false});
+
+  LanguageModel.fromJson(Map<String, dynamic> json)
+      : titleId = json['titleId'],
+        languageCode = json['languageCode'],
+        countryCode = json['countryCode'],
+        isSelected = json['isSelected'];
+
+  Map<String, dynamic> toJson() => {
+    'titleId': titleId,
+    'languageCode': languageCode,
+    'countryCode': countryCode,
+    'isSelected': isSelected,
+  };
+
+  @override
+  String toString() {
+    StringBuffer sb = new StringBuffer('{');
+    sb.write("\"titleId\":\"$titleId\"");
+    sb.write(",\"languageCode\":\"$languageCode\"");
+    sb.write(",\"countryCode\":\"$countryCode\"");
+    sb.write('}');
+    return sb.toString();
+  }
+}
+
+class SplashModel {
+  String title;
+  String content;
+  String url;
+  String imgUrl;
+
+  SplashModel({this.title, this.content, this.url, this.imgUrl});
+
+  SplashModel.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        content = json['content'],
+        url = json['url'],
+        imgUrl = json['imgUrl'];
+
+  Map<String, dynamic> toJson() => {
+    'title': title,
+    'content': content,
+    'url': url,
+    'imgUrl': imgUrl,
+  };
+
+  @override
+  String toString() {
+    StringBuffer sb = new StringBuffer('{');
+    sb.write("\"title\":\"$title\"");
+    sb.write(",\"content\":\"$content\"");
+    sb.write(",\"url\":\"$url\"");
+    sb.write(",\"imgUrl\":\"$imgUrl\"");
+    sb.write('}');
+    return sb.toString();
+  }
+}
