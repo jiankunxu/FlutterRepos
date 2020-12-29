a9776fefb36aaef745fecc66c942e187087a0dba
xujiankun <jiankunxu@126.com>
2020-12-29 13:47:52 +0800

    update

--- base_library/lib/src/common/common.dart
+++ base_library/lib/src/common/common.dart
@@ -8,5 +8,6 @@ class BaseConstant {
 
   static const String routeMain = 'route_main';
   static const String routeLogin = 'route_login';
+  static const String key_splash_model = 'key_splash_models';
   static const String key_theme_color = 'key_theme_color';
 }
